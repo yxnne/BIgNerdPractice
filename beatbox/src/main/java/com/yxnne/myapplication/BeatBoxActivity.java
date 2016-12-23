306e4bdf0bf03212e4c6079b4ba4e7c58335ec71
yxnne <506524339@qq.com>
2016-12-23 19:25:16 +0800

    big nerd ranch chapter 18 code A new project start !

---
+++ beatbox/src/main/java/com/yxnne/myapplication/BeatBoxActivity.java
@@ -0,0 +1,13 @@
+package com.yxnne.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beat_box);
+    }
+}
